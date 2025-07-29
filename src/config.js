f034f69f040fce040c1a201d38c78b61ad1012d6
Amrithesh <amritheshkakkoth10@gmail.com>
2025-07-30 00:36:15 +0530

    Projects had been added

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export const metaData = {
   baseUrl: "https://astrofolio-astro.vercel.app/",
-  title: "Astrofolio",
-  name: "Vikas",
+  title: "quasar1256",
+  name: "Amrithesh Kakkoth",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/vikas5914",
-  github: "https://github.com/vikas5914/Astrofolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  twitter: "https://x.com/AmritheshK",
+  github: "https://github.com/Amrithesh-Kakkoth",
+  instagram: "https://www.instagram.com/quasar1256/",
+  linkedin: "https://www.linkedin.com/in/amrithesh-kakkoth/",
+  email: "mailto:amritheshkakkothh.mec@gmail.com",
 };
